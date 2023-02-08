8065611bdd70e5eb961b29df95a567dbed075396
Vittorio D’Alfonso <vittoriodalfonso@Mac-Studio.Home>
2023-02-08 20:18:52 +0100

    Added Docker (Compose included)

--- Ubuntu 22.04/setup.sh
+++ Ubuntu 22.04/setup.sh
@@ -36,10 +36,12 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
 
 # Installiamo Docker (https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04)
-sudo apt install docker.io
-
-# installiamo docker-compose
-sudo apt install docker-compose
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
 
 # Aggiungiamo l'utente ubuntu al gruppo docker
 sudo usermod -aG docker ${USER}
